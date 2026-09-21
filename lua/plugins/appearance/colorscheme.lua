64059df88c18113ed1b29cde950d20d959ae30f6
chenggou <chenggou124@gmail.com>
2026-09-21 09:49:33 +0800

    feat: highlight consuming Rust values

--- lua/plugins/appearance/colorscheme.lua
+++ lua/plugins/appearance/colorscheme.lua
@@ -3,6 +3,14 @@ return {
 	name = "rose-pine",
 	priority = 1000,
 	config = function()
+		require("rose-pine").setup({
+			highlight_groups = {
+				["@lsp.mod.consuming.rust"] = {
+					bold = true,
+				},
+			},
+		})
+
 		vim.cmd("colorscheme rose-pine-moon")
 	end,
 }
